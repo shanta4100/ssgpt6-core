fd0c272ba572af852471724cfb124e6ca5af463e
Arifur Shanta <p888qd7jc8@privaterelay.appleid.com>
2026-03-13 13:32:36 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "SSGPT6 Core",
+  description: "Intelligent Digital Access Platform"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body style={{margin:0,fontFamily:"Arial"}}>
+        {children}
+      </body>
+    </html>
+  );
+}
